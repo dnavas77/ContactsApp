f8645e45062d46de5259abc148828883a2e6ee7f
dnavas77 <danilo.navas@outlook.com>
2018-06-27 01:07:52 -0400

    good progress

--- ContactsApp/ClientApp/app/components/contacts/form/form.component.ts
+++ ContactsApp/ClientApp/app/components/contacts/form/form.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Event } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'contact-form',
@@ -11,14 +11,15 @@ export class ContactFormComponent implements OnInit {
     @Input()
     actionType: string = "";
 
-    contactForm!: FormGroup;
+    invalidImage: boolean = false;
     photo: File | null = null;
+    contactForm!: FormGroup;
 
     ngOnInit() {
         this.contactForm = new FormGroup({
-            firstName: new FormControl(),
-            lastName: new FormControl(),
-            email: new FormControl(),
+            firstName: new FormControl('', Validators.required),
+            lastName: new FormControl('', Validators.required),
+            email: new FormControl('', Validators.required),
             phone: new FormControl(),
             birthday: new FormControl(),
             comments: new FormControl(),
@@ -28,16 +29,19 @@ export class ContactFormComponent implements OnInit {
     getFile(event: any) {
         this.photo = event.target.files[0];
         const type = this.photo ? this.photo.type : "";
-console.warn('222222')
-        if (type.indexOf("png") < 0 || type.indexOf("jpeg") < 0 || type.indexOf("gif") < 0) {
-console.warn('333333')
+        if (type.indexOf("png") < 0 && type.indexOf("jpeg") < 0 && type.indexOf("gif") < 0) {
             this.photo = null;
-            event.target.files = null;
+            this.invalidImage = true;
+        } else {
+            this.invalidImage = false;
         }
-
     }
 
     submitNewContact() {
+        if (this.contactForm.invalid || this.invalidImage) {
+            // Notify: there's errors in the form
+            return;
+        }
         console.warn('submitted..');
     }
 
