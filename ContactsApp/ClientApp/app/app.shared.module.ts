5a308d8b1f6fe94b469fff22d4ccda12ff4f3171
dnavas77 <danilo.navas@outlook.com>
2018-06-27 00:16:00 -0400

    good progress. almost posting the form

--- ContactsApp/ClientApp/app/app.shared.module.ts
+++ ContactsApp/ClientApp/app/app.shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -25,6 +25,7 @@ import { ContactFormComponent } from './components/contacts/form/form.component'
         CommonModule,
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
--- ContactsApp/ClientApp/app/components/contacts/form/form.component.ts
+++ ContactsApp/ClientApp/app/components/contacts/form/form.component.ts
@@ -1,13 +1,41 @@
-﻿import { Component, Input } from '@angular/core';
+﻿import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Event } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'contact-form',
     templateUrl: './form.component.html'
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
     @Input()
     actionType: string = "";
+
+    contactForm!: FormGroup;
+    photo: File | null = null;
+
+    ngOnInit() {
+        this.contactForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            phone: new FormControl(),
+            birthday: new FormControl(),
+            comments: new FormControl(),
+        });
+    }
+
+    getFile(event: any) {
+        this.photo = event.target.files[0];
+        const type = this.photo ? this.photo.type : "";
+console.warn('222222')
+        if (type.indexOf("png") < 0 || type.indexOf("jpeg") < 0 || type.indexOf("gif") < 0) {
+console.warn('333333')
+            this.photo = null;
+            event.target.files = null;
+        }
+
+    }
 
     submitNewContact() {
         console.warn('submitted..');
